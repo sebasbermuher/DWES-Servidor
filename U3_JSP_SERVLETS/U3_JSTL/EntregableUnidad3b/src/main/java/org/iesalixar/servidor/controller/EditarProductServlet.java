8bfd154ea76b40a58f95fef48081c9260f600708
sebasbermuher <alum.sbermudezh@iesalixar.org>
2021-11-22 20:32:05 +0100

    Entregable U3 B

---
+++ U3_JSP_SERVLETS/U3_JSTL/EntregableUnidad3b/src/main/java/org/iesalixar/servidor/controller/EditarProductServlet.java
@@ -0,0 +1,43 @@
+package org.iesalixar.servidor.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class EditarProductServlet
+ */
+@WebServlet("/EditarProductServlet")
+public class EditarProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditarProductServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
